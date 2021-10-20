ada3134dee45a7214e885697c93b7462d82b6b8e
Angel Chaile <angelchaile90@gmail.com>
2021-10-20 16:08:04 -0300

    cambios en enviar php y contacto html

--- enviar.php
+++ enviar.php
@@ -1,6 +1,7 @@
 <?php
 	use PHPMailer\PHPMailer\PHPMailer;
 	use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
 	//Se debe verificar que los archivos que se requieren a continuación se encuentren en las rutas indicadas
 	require 'PHPMailer/Exception.php';
 	require 'PHPMailer/PHPMailer.php';
@@ -12,7 +13,7 @@
     //Declaración de variables para almacenar los datos ingresados por el usuario en cada input del formulario. Recordar que se accede por el "name" del input.
 
     $nombre = $_POST['nombre'];
-    $nombre = $_POST['apellido']; //agregue el apellido (preguntar al profe)//
+    $apellido = $_POST['apellido']; //agregue el apellido (preguntar al profe)//
     $email = $_POST['correo'];
     $comentario = $_POST['comentario'];
 
@@ -22,14 +23,14 @@ try {
     $mail->isSMTP();       //Se utiliza el protocolo SMTP
     $mail->Host       = 'smtp.gmail.com';  //Colocar aquí el servidor de correo a utilizar, en el ejemplo smtp de gmail
     $mail->SMTPAuth   = true;     //Se habilita la autenticación smtp
-    $mail->Username   = 'chaileenzo195@gmail.com'; //Colocar aquí una dirección de correo valida, debe pertenecer al servidor indicado arriba
-    $mail->Password   = 'TobiNaruto05'; //Colocar aquí la contraseña
+    $mail->Username   = 'chaileangel91@gmail.com'; //Colocar aquí una dirección de correo valida, debe pertenecer al servidor indicado arriba
+    $mail->Password   = '123498765'; //Colocar aquí la contraseña
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Habilita el cifrado TLS; se recomienda `PHPMailer::ENCRYPTION_SMTPS` 
     $mail->Port       = 587;                                    //Número del puerto utilizado
 
  
-    $mail->setFrom('chaileenzo195@gmail.com', 'Mi Pagina Web'); //Desde donde se envía el mail, el nombre es opcional
-    $mail->addAddress('chaileangel91@gmail.com', 'Mensaje hacia mi');     //A quién se le envía el mail, el nombre es opcional
+    $mail->setFrom('chaileangel91@gmail.com', 'Mi Pagina Web'); //Desde donde se envía el mail, el nombre es opcional
+    $mail->addAddress('chaileenzo195@gmail.com', 'Mensaje hacia mi');     //A quién se le envía el mail, el nombre es opcional
     //$mail->addAddress('ellen@example.com');  //información opcional
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('cc@example.com');
@@ -42,11 +43,15 @@ try {
     //Contenido
     $mail->isHTML(true);                     //Si se envía con formato HTML
     $mail->Subject = 'Asunto Consulta';  //Asunto del mensaje
-    $mail->Body    = 'Mensaje desde R.G Rusticos'; //Mensaje a enviar
+    $mail->Body    = 'De:'.$email."<br>".
+                     'De:'.$nombre."<br>".
+                     'De:'.$nombre."<br>".
+                    'Mensaje: '.$comentario; //Mensaje a enviar
  
 
     $mail->send(); //Se envía el mail
     echo "Gracias por contactarnos, responderemos a la brevedad"; //Fin del try
+    echo "<br><input type=\"button\" onclick=\"location.href='".(isset($_SERVER['HTTPS']) ? 'https://' : 'http//') .$_SERVER['HTTP_HOST']."'\" value=\"Volver\"></button>";
 } catch (Exception $e) {
     echo "Error, el mensaje no se envió: {$mail->ErrorInfo}"; //Si hay algún error
 }
